4643adbf3406581b6d267c4fbafeb712fa36437a
sandeep <sandeep3587@gmail.com>
2025-07-15 00:30:33 +0530

    add post method for sending the payload as a xml or json format

--- SB_Rest_Api_02/src/main/java/org/example/sb_rest_api_02/controller/CustomerController.java
+++ SB_Rest_Api_02/src/main/java/org/example/sb_rest_api_02/controller/CustomerController.java
@@ -1,7 +1,11 @@
 package org.example.sb_rest_api_02.controller;
 
 import org.example.sb_rest_api_02.binding.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +20,16 @@ public class CustomerController {
         customer.setGender("male");
 
         return customer;
+    }
+
+    @PostMapping(value = "/customer",
+    consumes = {"application/xml","application/json"},
+    produces = {"text/plain"})
+    public ResponseEntity<String> getCustomerResponse(@RequestBody Customer customer) {
+
+        System.out.println(customer);
+
+        return new ResponseEntity<>("Customer Saved", HttpStatus.CREATED);
+
     }
 }
